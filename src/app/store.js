fede46fa2a7531bd97657922a885b9aacadc5699
rosen <you@example.com>
2022-01-09 17:09:33 +0700

    config with redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from 'features/SignInSignUp/userSlice'
 import cartReducer from 'features/Cart/cartSlice'
-const rootReducer = {
+import {
+    persistStore, persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+const rootReducer = combineReducers({
     users: userReducer,
     cart: cartReducer,
-}
-const store = configureStore({
-    reducer:rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-   
 })
-export default store
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export const persistor = persistStore(store);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from 'app/store';
+import store, { persistor } from 'app/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>,
   </React.StrictMode>,
 
   document.getElementById('root')
